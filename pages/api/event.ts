a379dc255575723b161ad793ec74625d6a8b0655
CostaNj <costa.n.work@yandex.ru>
2020-07-17 14:38:41 +0300

    use API routes

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest , NextApiResponse} from 'next'
 
-export default (req, res) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log('API: ', req.query)
   res.statusCode = 200
   res.json({ name: req?.query?.id })
