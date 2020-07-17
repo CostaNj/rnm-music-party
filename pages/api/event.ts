03515c30537fbed2ea1207f0d11a097959017617
CostaNj <costa.n.work@yandex.ru>
2020-07-17 14:41:46 +0300

    use API routes for event

--- pages/api/event.ts
+++ pages/api/event.ts
@@ -1,8 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest , NextApiResponse} from 'next'
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+interface EventApiRequest extends NextApiRequest {
+  query: {
+    id: string
+  }
+}
+
+export default (req: EventApiRequest, res: NextApiResponse) => {
   console.log('API: ', req.query)
   res.statusCode = 200
-  res.json({ name: req?.query?.id })
+  res.json({ name: req.query.id })
 }
